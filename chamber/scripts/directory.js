9303769f45e8e95e54d1bff064b97926c1e17493
Matheus Felber <math.felber@gmail.com>
2024-06-22 15:40:21 -0300

    chamber of commerce

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -17,7 +17,7 @@ display.classList.toggle("grid", view === "grid");
 display.classList.toggle("list", view === "list");
 }
 /* CREATE CARDS FROM API */
-const urlBusiness = "https://mattfelber.github.io/wdd230/chamber/data.json"
+const urlBusiness = "https://mattfelber.github.io/wdd231/chamber/data.json"
 const checkDirectory = document.querySelector(".directory-main article");
 
 async function getBusinessData() {
