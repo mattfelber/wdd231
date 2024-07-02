844833e794df931f1ac4228450744e1dd16a3127
Matheus Felber <math.felber@gmail.com>
2024-07-02 19:49:11 -0300

    dialog

---
+++ startLab/js/scripts.js
@@ -0,0 +1,12 @@
+const openButton = document.querySelector("#openButton");
+const dialogBox = document.querySelector("#dialogBox");
+const closeButton = document.querySelector("#closeButton");
+
+
+openButton.addEventListener("click", () => {
+    dialogBox.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    dialogBox.close();
+})
